4dc7fa141f4adf83a809027e73c39867399e9b47
zackmon99 <monzack4@gmail.com>
2020-07-24 14:36:24 -0700

    Challenge Complete

--- CriminalIntentRecyclerViewViewtypes/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ CriminalIntentRecyclerViewViewtypes/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,7 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import java.util.*
-
+// TEST
 data class Crime (  val id: UUID = UUID.randomUUID(),
                     var title: String = "",
                     var date: Date = Date(),
