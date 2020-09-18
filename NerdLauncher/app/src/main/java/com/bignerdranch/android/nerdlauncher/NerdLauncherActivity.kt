8a1ad19f76bd1e30a51adb3753b376f4fc2afcdd
zackmon99 <monzack4@gmail.com>
2020-09-17 20:10:15 -0700

    End of NerdLauncher and start of PhotoGallery

---
+++ NerdLauncher/app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.nerdlauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+    }
+}
